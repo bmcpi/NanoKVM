5f0abede14f9c1958f966f1d373d99ec3705e3eb
appkins <nbatkins@gmail.com>
2026-05-07 18:20:43 -0500

    Fix templates

--- server/templates/layout_templ.go
+++ server/templates/layout_templ.go
@@ -5,10 +5,8 @@ package templates
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
 // layout renders the common HTML document shell for BMC pages.
 // Includes Tailwind CSS via CDN and a dark theme base.
